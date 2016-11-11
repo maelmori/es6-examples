19d485e2e9f2bd6ace895950bf1aa8365717c8c8
maelmori@gmail.com <maelmori@gmail.com>
2016-11-11 16:12:43 +0100

    final structure

--- examples/maps_set/004_weakset.js
+++
@@ -1,9 +0,0 @@
-// weakset works roughly the same way
-const weakset = new WeakSet()
-let today = new Date()
-weakset.add(today)
-console.log(weakset.has(today))
-// Oooops
-today = null
-// Bye bye
-console.log(weakset.has(today))
--- examples/modules/modules_001.js
+++
@@ -1,6 +0,0 @@
-// Still failing in lot of parsers
-import * as math from 'lib/math'
-console.log('2π = ' + math.sum(math.pi, math.pi))
-
-import { sum, pi } from 'lib/math'
-console.log('2π = ' + sum(pi, pi))
