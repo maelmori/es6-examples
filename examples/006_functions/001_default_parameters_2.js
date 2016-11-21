2b197a799fae4e51498ca8168b45c90f5d46acc9
maelmori@gmail.com <maelmori@gmail.com>
2016-11-21 13:01:02 +0100

    some minor changes

--- examples/006_functions/001_default_parameters_2.js
+++ examples/006_functions/001_default_parameters_2.js
@@ -1,11 +1,4 @@
-// Deestructing default parameters
-// careful: equal signs instead of semicolon.
-// This is still buggy in firefox
-function drawObject ({shape = 'circle', radius = 5, position = {x: 0, y: 0}}) {
-  console.log(radius)
-}
-
-drawObject({})
+// Destructing default parameters
 
 const geometryObject = {
   shape: 'circle',
@@ -16,4 +9,11 @@ const geometryObject = {
   }
 }
 
+// careful: equal signs instead of semicolon.
+// This is still buggy in firefox
+function drawObject ({shape = 'circle', radius = 5, position = {x: 0, y: 0}}) {
+  console.log(radius)
+}
+
+drawObject({})
 drawObject(geometryObject)
