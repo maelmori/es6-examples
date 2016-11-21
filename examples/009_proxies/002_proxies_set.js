e605db55d1f2b55bec0644285bfc800cf4153c49
maelmori@gmail.com <maelmori@gmail.com>
2016-11-21 13:01:02 +0100

    some minor changes

--- examples/009_proxies/002_proxies_set.js
+++ examples/009_proxies/002_proxies_set.js
@@ -6,14 +6,14 @@ const myObject = {
 
 const myProxy = new Proxy(myObject, {
   set: function (target, key, value, receiver) {
-    if (key != 'myProperty') {
-      console.log("Please, you can only set myProperty")
+    if (key !== 'myProperty') {
+      console.log('Please, you can only set myProperty')
     } else {
       Reflect.set(target, key, value, receiver)
     }
   }
 })
 
-myProxy.anyOtherThing = "Test"
+myProxy.anyOtherThing = 'Test'
 myProxy.myProperty = 10
 console.log(myProxy)
