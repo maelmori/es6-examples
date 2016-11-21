f43acec47b51e19bf8bc1da7b7d710eda1ff9d07
maelmori@gmail.com <maelmori@gmail.com>
2016-11-21 17:15:12 +0100

    some minor changes

--- examples/003_interpolation/003_raw_property.js
+++ examples/003_interpolation/003_raw_property.js
@@ -1,7 +1,23 @@
-// String. repeat
-console.log('pepino ' + 'ino '.repeat(5))
+// Raw property and String method
 
-// String search
-console.log('pepino'.startsWith('pep'))
-console.log('pepino'.endsWith('ino'))
-console.log('pepino'.includes('pepi'))
+const name = 'Juanillo'
+
+const order = {
+  quantity: 5,
+  pricePerUnit: 4.5,
+  product: 'cervezas ',
+  productTrademark: 'Paulaner'
+}
+
+function myES6TemplateFunction (strings, ...values) {
+  // As a property, we can access to the original string
+  console.log(strings.raw)
+}
+
+myES6TemplateFunction`${name} se ha pimplado ${ order.quantity } de esas ${order.product}`
+
+// The raw method of the String object works like a template function
+const txt = String.raw`${name} se ha pimplado ${ order.quantity } de esas ${order.product}`
+console.log(txt)
+// Wich is more or less the same
+console.log(`${name} se ha pimplado ${ order.quantity } de esas ${order.product}`)
