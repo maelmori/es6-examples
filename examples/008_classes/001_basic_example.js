9c8c710e47c96c52839a187da25190727ca18c02
maelmori@gmail.com <maelmori@gmail.com>
2016-11-21 13:01:02 +0100

    some minor changes

--- examples/008_classes/001_basic_example.js
+++ examples/008_classes/001_basic_example.js
@@ -1,4 +1,18 @@
-// Base class
+// The classical ES5 class
+
+function ES5Shape (id, x, y) {
+  this.id = id
+  this.move(x, y)
+}
+ES5Shape.prototype.move = function (x, y) {
+  this.x = x
+  this.y = y
+}
+
+var s2 = new ES5Shape(1, 10, 30)
+s2.move(1, 20)
+
+// The ES6 Class equivalent
 
 class Shape {
   constructor (id, x, y) {
@@ -13,3 +27,8 @@ class Shape {
 
 const s = new Shape(1, 10, 30)
 s.move(1, 20)
+
+// Don't be fooled by the syntax
+// It's almost the same function object as always
+
+console.log(typeof Shape) // surprise!
