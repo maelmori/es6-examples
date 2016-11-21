f5607adcdfb4cc590a073bc8c8642bf8b936eb57
maelmori@gmail.com <maelmori@gmail.com>
2016-11-21 17:32:25 +0100

    some additions

--- examples/003_interpolation/004_new_string_methods.js
+++ examples/003_interpolation/004_new_string_methods.js
@@ -5,3 +5,16 @@ console.log('pepino ' + 'ino '.repeat(5))
 console.log('pepino'.startsWith('pep'))
 console.log('pepino'.endsWith('ino'))
 console.log('pepino'.includes('pepi'))
+
+// Those new methods are not strictly of the String object
+// but they are about formatting numbers in strings
+
+let formatter = new Intl.NumberFormat('es-ES')
+console.log(formatter.format(1234567.89)) // 1,234,567.89
+
+formatter = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' })
+console.log(formatter.format(100200300.40)) // € 100,200,300.40
+
+console.log(['a', 'á', 'n', 'ñ', 'z'].sort()) // [ 'a', 'n', 'z', 'á', 'ñ' ]
+let esCollator = new Intl.Collator('es').compare
+console.log(['a', 'á', 'n', 'ñ', 'z'].sort(esCollator)) // [ 'a', 'á', 'n', 'ñ', 'z' ]
